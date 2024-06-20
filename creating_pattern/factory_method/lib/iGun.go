842d9482d4f643f509835a951ee69ccc4bbebb8c
JJJayw <271753416@qq.com>
2024-06-20 16:13:28 +0800

    feat: 新增工厂方法

---
+++ creating_pattern/factory_method/lib/iGun.go
@@ -0,0 +1,8 @@
+package lib
+
+type IGun interface {
+	SetName(name string)
+	SetPower(power int)
+	GetName() string
+	GetPower() int
+}
---
+++ creating_pattern/factory_method/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"go-design-patterns/creating_pattern/factory_method/lib"
+)
+
+func main() {
+	ak47, _ := lib.GetGun("ak47")
+	musket, _ := lib.GetGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g lib.IGun) {
+	fmt.Printf("Gun: %s", g.GetName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.GetPower())
+	fmt.Println()
+}
